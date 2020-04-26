09ba3c33c524a5a33c4be65bf892c956b52730ca
Duan Rafael Ribeiro <duan.ribeiro@hotmail.com>
2020-04-26 18:08:10 -0300

    Minor,

--- src/components/MapChart/index.js
+++ src/components/MapChart/index.js
@@ -1,33 +1,60 @@
 import React, { useState, useEffect } from "react";
-import L from 'leaflet'
+import L, { icon } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import "./styles.scss"
 import styled from 'styled-components'
+import { bela_vista_polygon } from './../../maps/sao_paulo_districts'
+import axios from 'axios'
 
 const Wrapper = styled.div`
   width: ${props => props.width}
   height: ${props => props.height}
 `
 
-
 export default function MapChart() {
+  const position_1 = new L.LatLng(-23.562, -46.6434)
+
+
+
+
+  // useEffect(() => {
+  //   axios.get(`http://127.0.0.1:5000/api/v1/auth/get_district`)
+  //   .then(res => {
+  //     const persons = res.data;
+  //     this.setState({ persons });
+  //   })
+  // }, [])
+
+
+
+  
   useEffect(() => {
+    // Pré configurações
     let map = L.map('map', {
-      zoom: 18,
+      zoom: 14,
       zoomControl: false
     })
-
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 20,
         maxNativeZoom: 17
     }).addTo(map)
 
-    map.panTo(new L.LatLng(-23.564902, -46.647170))
+
+    // Desenhando o bairro
+    // L.polygon(bela_vista_polygon).addTo(map)
+    var myIcon = L.icon({
+      iconUrl: 'my-icon.png',
+      iconSize: [38, 95],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+      shadowUrl: 'my-icon-shadow.png',
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94]
+  });
+    L.marker([-23.572216, -46.645222]).addTo(map)
+    map.panTo(position_1)
 
   }, [])
-
-    
-
 
   return (
     <Wrapper width="1200px" height="450px" id="map"/>
---
+++ src/maps/sao_paulo_districts.js
@@ -0,0 +1,11 @@
+import L from 'leaflet'
+
+export const bela_vista_polygon = [
+    new L.LatLng(-23.575009, -46.641456),
+    new L.LatLng(-23.572146, -46.643023),
+    new L.LatLng(-23.560813, -46.656988),
+    new L.LatLng(-23.559860, -46.656022),
+    new L.LatLng(-23.554883, -46.655314),
+    new L.LatLng(-23.548717, -46.638942),
+    new L.LatLng(-23.552267, -46.636732)
+]
