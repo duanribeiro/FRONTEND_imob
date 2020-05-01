2c33299adb0e7f0522d50632d66cbd5c54c8a767
Duan Rafael Ribeiro <duan.ribeiro@hotmail.com>
2020-05-01 11:43:09 -0300

    Minor, refactor maps and icons code

--- src/components/MapChart/index.js
+++ src/components/MapChart/index.js
@@ -1,64 +1,54 @@
-import React, { useState, useEffect } from "react";
-import L, { icon } from 'leaflet'
+import React from "react";
 import 'leaflet/dist/leaflet.css'
 import "./styles.scss"
-import styled from 'styled-components'
-import { bela_vista_polygon } from './../../maps/sao_paulo_districts'
+import { LeafletMap } from './make_map'
 import axios from 'axios'
 
-const Wrapper = styled.div`
-  width: ${props => props.width}
-  height: ${props => props.height}
-`
 
 export default function MapChart() {
-  const position_1 = new L.LatLng(-23.562, -46.6434)
+  const [map, setMap] = React.useState()
+  const [results, setResults] = React.useState({
+    "bank": [],
+    "gas_station": [],
+    "gym": [],
+    "school": [],
+    "shopping_mall": [],
+    "subway_station": [],
+  })
 
 
+  const callAPI = () => {
+    axios.get(`http://127.0.0.1:5000/api/v1/auth/get_district`)
+      .then(response => {
+        setResults(response.data)
+      })
+  }
 
-
-  // useEffect(() => {
-  //   axios.get(`http://127.0.0.1:5000/api/v1/auth/get_district`)
-  //   .then(res => {
-  //     const persons = res.data;
-  //     this.setState({ persons });
-  //   })
-  // }, [])
-
-
-
-  
-  useEffect(() => {
-    // Pré configurações
-    let map = L.map('map', {
-      zoom: 14,
-      zoomControl: false
+  React.useEffect(() => {
+    results["school"].forEach(element => {
+      map.makeIcon([
+        element["geometry"]["location"]["lat"],
+        element["geometry"]["location"]["lng"]
+        ],
+        'red')
     })
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20,
-        maxNativeZoom: 17
-    }).addTo(map)
 
+    results["subway_station"].forEach(element => {
+      map.makeIcon([
+        element["geometry"]["location"]["lat"],
+        element["geometry"]["location"]["lng"]
+        ],
+        'blue')
+    })
+  }, [results])
 
-    // Desenhando o bairro
-    // L.polygon(bela_vista_polygon).addTo(map)
-    var myIcon = L.icon({
-      iconUrl: 'my-icon.png',
-      iconSize: [38, 95],
-      iconAnchor: [22, 94],
-      popupAnchor: [-3, -76],
-      shadowUrl: 'my-icon-shadow.png',
-      shadowSize: [68, 95],
-      shadowAnchor: [22, 94]
-  });
-    L.marker([-23.572216, -46.645222]).addTo(map)
-    map.panTo(position_1)
-
+  React.useEffect(() => {
+    setMap(new LeafletMap([-23.564942, -46.647168], 15))
+    callAPI()
   }, [])
 
   return (
-    <Wrapper width="1200px" height="450px" id="map"/>
-      
+    <div className="map" id="map"/>
   );
 }
 
---
+++ src/components/MapChart/make_map.js
@@ -0,0 +1,35 @@
+import L from 'leaflet'
+import { bela_vista_polygon } from './../../maps/sao_paulo_districts'
+
+export class LeafletMap {
+    constructor(initial_position, initial_zoom) {
+      this.initial_position = initial_position
+      this.initial_zoom = initial_zoom
+      this.map = L.map('map').setView(initial_position, initial_zoom)
+
+      L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png').addTo(this.map)
+    
+      // Desenhando o bairro  
+      L.polygon(bela_vista_polygon).addTo(this.map)
+    }
+    
+    makeIcon(icon_position, icon) {
+      let LeafIcon = L.Icon.extend({
+        options: {
+          iconSize: [30, 30],
+          iconAnchor: [10, 33]
+        }
+      })
+    
+      let icon_url = ''
+      if (icon == 'red'){
+        icon_url = 'http://icons.veryicon.com/128/System/Small%20%26%20Flat/map%20marker.png'
+      } else if (icon == 'blue') {
+        icon_url = 'https://image.flaticon.com/icons/svg/2642/2642502.svg'
+      }
+
+  
+      let myIcon = new LeafIcon({iconUrl: icon_url})
+      L.marker(icon_position,  {icon: myIcon}).bindPopup("Parque Fluvial Renato Poblete").addTo(this.map)
+    }
+  }
