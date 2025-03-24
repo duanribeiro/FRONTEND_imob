213b8fc75a27e071249d35394b7f5650c17540c2
Duan Ribeiro <duan.ribeiro@shopee.com>
2025-03-24 10:12:20 -0300

    fix: wrong price_id

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -153,7 +153,7 @@ export const createCheckoutSession = async (customerID: string, email: string | 
         customer_id: customerID,
         email: email,
         // price_id: "price_1QPPFxCKdNLiy4hX7sV6pwcW",
-        price_id: "prod_RvQc4x5grCjqeg",
+        price_id: "price_1R1ZlOCKdNLiy4hXgCKtSS9A",
         success_url: `${window.location.origin}/radar`,
         cancel_url: `${window.location.origin}/radar`,
       }),
